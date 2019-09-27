d34766528a11451695a83ac43bd49dfe4c3efefd
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2019-09-27 15:43:01 -0400

    show board

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+board = [
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+    ["p", "p", "p", "p", "p", "p", "p", "p"],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+]
+
+def show_board():
+    for row in board:
+        for piece in row:
+            print(piece, end=" ")
+        print()
+    print()
+
+turn = "White"
+
 while True:
-    user_move = input(":")
-    print("Move: " + user_move)
+    show_board()
+    user_move = input("Move : ")
+    print("Your move: " + user_move)
